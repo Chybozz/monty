fe586393e709b1640cec0b03ce8a22ac6f1a2032
chybozz <134920121+Chybozz@users.noreply.github.com>
2023-12-14 18:11:36 -0800

    Div Opcode

--- div.c
+++ div.c
@@ -16,7 +16,7 @@ void f_div(stack_t **head, unsigned int counts)
 		h = h->next;
 		len++;
 	}
-	if (len < 2)
+	if (!(len >= 2))
 	{
 		fprintf(stderr, "L%d: can't div, stack too short\n", counts);
 		fclose(bus.file);
